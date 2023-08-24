a42c007a048b10d20f5aa4644b55cf74c3fd1097
sharad2512 <shnarute95@gmail.com>
2023-08-24 22:03:31 +0530

    Added fundoo note home page

--- JS/logInPage.js
+++ JS/logInPage.js
@@ -1,6 +1,7 @@
-window.onload = function(){
-    let form = document.getElementById('form1')
-    form.addEventListener('submit', function(event){
+window.onload = function () {
+
+    let form = document.getElementById('form-1')
+    form.addEventListener('submit', function (event) {
         event.preventDefault()
         let email = document.getElementById('email').value
         let password = document.getElementById('password').value
@@ -9,11 +10,33 @@ window.onload = function(){
         } if (password === "") {
             document.getElementById('passErr').innerHTML = "password field is required"
         }
+        // if(!emailValidation(email)){
+        //     document.getElementById('emailErr').innerHTML = "enter currect email"
+        // }
         let allData = {
             email: email,
             password: password
         }
         console.log(allData)
+        $.ajax({
+            url: "http://127.0.0.1:8000/api/user/login",
+            type: "POST",
+            data: allData,
+            success: function (result) {
+                console.log("Login Successfully")
+                console.log(result);
+                // let token = result.data;
+                // localStorage.setItem('token',token);
+                // window.location.href = 'http://127.0.0.1:5500/pages/dashboard.html'
+            }
+        })
     })
+    //   function  emailValidation (email){
+    //     let  emailRegex = /^[a-z]{3,}(.[0-9a-z])?@([a-z]){2,}.[a-z]$/;
+    //     // let passwordRegex = /^.(?=.{8,})(?=.[A-Z])(?=.[0-9])(?=.[@#$%^&+=]).*$/;
+    //    let validation = emailRegex.test(email)
+    //    console.log(validation)
+    // }
+
 }
 
--- JS/signUpPage.js
+++ JS/signUpPage.js
@@ -1,32 +1,56 @@
-window.onload = function(){
-let form = document.getElementById('form-1')
-form.addEventListener('submit', function(event){
-    event.preventDefault()
-    let firstName = document.getElementById('first_name').value
-    let lastName = document.getElementById('last_name').value
-    let email = document.getElementById('email').value
-    let password = document.getElementById('password').value
-    let confirmPassword = document.getElementById('password_confirm').value
-    if(firstName === ""){
-        document.getElementById('first').innerHTML= "first name field is required"
-    } 
-    if(lastName === ""){
-        document.getElementById('last').innerHTML= "last name field is required"
-    } if(email === ""){
-        document.getElementById('emailErr').innerHTML= "email field is required"
-    } if(password === ""){
-        document.getElementById('pass').innerHTML= "password field is required"
-    } if(confirmPassword == ""){
-        document.getElementById('conf').innerHTML= "confirm password field is required"
-    }
-    
-    let allData = {
-        name:firstName+" "+lastName,
-        email:email,
-        password:password
-    }
-    console.log(allData)
+window.onload = function () {
+    let form = document.getElementById('form-1')
+    form.addEventListener('submit', function (event) {
+        event.preventDefault()
+        let firstName = document.getElementById('first_name').value
+        let lastName = document.getElementById('last_name').value
+        let email = document.getElementById('email').value
+        let password = document.getElementById('password').value
+        let confirmPassword = document.getElementById('password_confirm').value
 
-})
+        if (firstName === "") {
+            document.getElementById('first').innerHTML = "first name field is required"
+        } if (lastName === "") {
+            document.getElementById('last').innerHTML = "last name field is required"
+        } if (email === "") {
+            document.getElementById('emailErr').innerHTML = "email field is required"
+        } if (password === "") {
+            document.getElementById('pass').innerHTML = "password field is required"
+        } if (confirmPassword === "") {
+            document.getElementById('conf').innerHTML = "confirm password field is required"
+        }
+        //email validation
+        // if(emailValidation(email)){
+        //     document.getElementById('emailErr').innerHTML = "enter currect email"
+        // }
+
+        let allData = {
+            name: firstName + " " + lastName,
+            email: email,
+            password: password,
+            password_confirmation: confirmPassword
+        }
+        console.log(allData)
+        
+        // function  emailValidation (email){
+        //     let  emailRegex = /^[a-z]{3,}(.[0-9a-z])?@([a-z]){2,}.[a-z]$/;
+        //     // let passwordRegex = /^.(?=.{8,})(?=.[A-Z])(?=.[0-9])(?=.[@#$%^&+=]).*$/;
+        //    let validation = emailRegex.test(email)
+        //    console.log(validation)
+        // }
+        $.ajax({
+            url:"http://127.0.0.1:8000/api/user/signUp",
+            type:"post",
+            data:allData,
+            success: function(result){
+                console.log("sign-up Successfully")
+                console.log(result);
+                // let token = result.data;
+                // localStorage.setItem('token',token);
+                // window.location.href = 'http://127.0.0.1:5500/pages/dashboard.html'
+            }
+        })
+
+    })
+    
 }
- 
